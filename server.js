7cde4d473cfa1de4432b60a7aa2a2bf89d05af66
Den <den@mobdome.com>
2026-02-08 01:14:22 +0000

    Add MVP code

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-const express = require('express'); const app = express(); app.use(express.static('public')); app.listen(3002, () => console.log('Social server on 3002'));
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3002;
+app.use(express.static('public'));
+app.get('/', (req, res) => res.send('Mobdome Social MVP'));
+app.listen(port, () => console.log('Social running on ' + port));
